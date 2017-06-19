7ca5bc6cfb85622bf1924396513db0b184d9d16e
Sang <changocodon@gmail.com>
2017-06-19 23:44:48 +0700

    Add project

---
+++ CurriculumVitaeOnline/src/main/java/com/cvo/CurriculumVitaeOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.cvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumVitaeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumVitaeOnlineApplication.class, args);
+	}
+}
